14fc968c1cf0d44e471d34a33e6203a4359cbba7
kadirzkaya <bmkozkaya@gmail.com>
2022-11-04 14:22:36 +0300

    Book Order

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Book {
+import java.util.Date;
+
+public class Book implements Comparable<Book>{
+    private String name;
+    private int pageNumber;
+    private String authorName;
+    private Date publishDate;
+
+    public Book(String name, int pageNumber, String authorName, Date publishDate) {
+        this.name = name;
+        this.pageNumber = pageNumber;
+        this.authorName = authorName;
+        this.publishDate = publishDate;
+    }
+
+    @Override
+    public int compareTo(Book o) {
+        return this.getName().compareTo(o.getName());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        HashSet<Book> bookList=new HashSet<>();
+        bookList.add(new Book("Gülün Adı",123,"Umberto Eco",new Date("12/08/1977")));
+        bookList.add(new Book("Duman",235,"Ivan Turgenyev",new Date("12/08/1967")));
+        bookList.add(new Book("Beş Şehir",473,"Ahmet Hamdi Tanpınar",new Date("10/08/1966")));
+        bookList.add(new Book("Kar",344,"Orhan Pamuk",new Date("12/10/2000")));
+        bookList.add(new Book("Hayvan Çiftliği",112,"George Orwell",new Date("02/08/1937")));
+
+        TreeSet<Book> orderBooks=new TreeSet<>(bookList);
+
+        for(Book b:orderBooks){
+            System.out.println(b.getName());
+        }
+        System.out.println("------------------");
+        TreeSet<Book> pageOrderBooks=new TreeSet<>(new Comparator<Book>() {
+            @Override
+            public int compare(Book o1, Book o2) {
+                return o1.getPageNumber()-o2.getPageNumber();
+            }
+        });
+        pageOrderBooks.addAll(orderBooks);
+
+        for(Book b:pageOrderBooks){
+            System.out.println(b.getName()+" "+b.getPageNumber());
+        }
+    }
 }
